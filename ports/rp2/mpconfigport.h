6507f0e483685a1ffb2ab45667f1ca8a310f5412
james-holdsworth <james@rocinante.localdomain>
2022-03-07 14:55:28 +1100

    Enabled REPL over UART in ports/rp2 as per SDK documentation

--- ports/rp2/mpconfigport.h
+++ ports/rp2/mpconfigport.h
@@ -35,7 +35,7 @@
 
 // Board and hardware specific configuration
 #define MICROPY_HW_MCU_NAME                     "RP2040"
-#define MICROPY_HW_ENABLE_UART_REPL             (0) // useful if there is no USB
+#define MICROPY_HW_ENABLE_UART_REPL             (1) // useful if there is no USB
 #define MICROPY_HW_ENABLE_USBDEV                (1)
 
 #ifndef MICROPY_CONFIG_ROM_LEVEL
